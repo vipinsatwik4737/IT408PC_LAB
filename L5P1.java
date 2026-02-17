ad29ab0b1b12ff49fad28a9847d33714f1b7e6b9
vipinsatwik4737 <vipinsatwikr@gmail.com>
2026-02-17 15:11:49 +0530

    Add files via upload

---
+++ L5P1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class AgeException extends Exception {
+    public AgeException(String msg) {
+        super(msg);
+    }
+}
+
+
+public class ExceptionMain {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        try {
+            int age = s.nextInt();
+            if (age < 18) {
+                throw new AgeException("Sorry you are not eligible");
+            } else {
+                System.out.println("Please cast your vote");
+            }
+        } catch (AgeException ae) {
+            System.out.println(ae.getMessage());
+        }
+        s.close();
+    }
+}
