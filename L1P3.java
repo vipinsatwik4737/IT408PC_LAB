630fbfd28f6b10f9dacc42cdef94d83195cbf4b8
vipinsatwik4737 <vipinsatwikr@gmail.com>
2026-02-17 14:04:40 +0530

    Add files via upload

---
+++ L1P3.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+class L1P3{
+	public static void main(String args[]){
+		int x=10;
+		double D=15.18;
+		float y=(float)D;
+		double d=100.50;
+		char ch='a';
+		Boolean b=true;
+		byte by=6;
+		short s=12;
+		long l=55;
+		System.out.println("Integer value "+ x +"\nfloat value "+ d +"\ncharacter value "+ ch +"\nBoolean value "+ b +"\nbyte value "+ by +"\nshort value "+ s +"\nlong value "+ l +" ");
+		}
+	}
