c8073d7bfb8048b503cdd0d51c51bf7ff45fbb93
vipinsatwik4737 <vipinsatwikr@gmail.com>
2026-02-17 14:02:48 +0530

    Add files via upload

---
+++ L1P2.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+class L1P2{
+	public static void main(String args[]){
+		int x=18;
+		double d=x;
+		System.out.println("Integer value "+ x +" Double value "+ x +" ");
+		double D=50.765;
+		int y=(int) D;
+		System.out.println("Double value "+ y +" Integer value "+ y +" ");
+		}
+	}
